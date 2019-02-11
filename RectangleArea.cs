4904300b10bd503b2363994f0a05145d5a72c47c
lentqq <47202527+lentqq@users.noreply.github.com>
2019-02-11 14:54:53 +0200

    Add files via upload

---
+++ RectangleArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = decimal.Parse(Console.ReadLine());
+            var b = decimal.Parse(Console.ReadLine());
+            var rectangle = a * b;
+
+            Console.WriteLine("rectangle area is:"+rectangle);
+        }
+    }
+}
---
+++ SquareOfStars.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_of_stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('*',n));
+            Console.WriteLine(n);
+            for (int i = 0; i < (n-2); i++) ;
+            {
+                Console.Write('*');
+                for (int j = 0; j < n - 2; j++) ;
+                Console.Write('*');
+                Console.Write('\n');
+            }
+            Console.WriteLine(new string('*', n));
+            Console.WriteLine(n);
+            Console.Write('\n');
+        }
+    }
+}
